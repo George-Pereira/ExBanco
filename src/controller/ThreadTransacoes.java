fd8b6e009c1b919e7250fb9be9fcfb6d65800bcf
Fernando <fernanr8@gmail.com>
2019-03-22 10:48:53 -0300

    Alterações nos semaforos

--- src/controller/ThreadTransacoes.java
+++ src/controller/ThreadTransacoes.java
@@ -1,6 +1,69 @@
 package controller;
 
+import java.text.NumberFormat;
+import java.util.Locale;
+import java.util.concurrent.Semaphore;
+
 public class ThreadTransacoes extends Thread 
 {
 	
+	private int idTransacao;
+	private double valor;
+	private double saldo;
+	private static int [] transacao = new int [2];
+	private Semaphore semaforo;
+	
+	public ThreadTransacoes(int idTransacao, double valor, double saldo, Semaphore semaforo)
+	{
+		
+		this.idTransacao = idTransacao;
+		this.valor = valor;
+		this.saldo = saldo;
+		this.semaforo = semaforo;
+		
+	}
+	@Override
+	public void run() 
+	{
+		try 
+		{
+			semaforo.acquire();
+			if(transacao[idTransacao] == 0) 
+			{
+				transacao[idTransacao]++;
+				processaTransacao();
+			}
+		} 
+		catch (InterruptedException e) 
+		{
+			e.printStackTrace();
+		}
+		finally	
+		{
+			transacao[idTransacao]--;
+			semaforo.release();
+		}
+	}
+	private void processaTransacao() 
+	{
+		Locale local = new Locale("pt", "BR");
+		NumberFormat nf = NumberFormat.getCurrencyInstance(local);
+		switch(idTransacao) 
+		{
+			case 1: if(saldo > valor) 
+					{
+						saldo -= valor;
+						System.out.println("Novo Balanço da Conta = " + nf.format(saldo) + " após o saque de " + nf.format(valor));
+					}
+					else 
+					{
+						System.out.println("Saldo insuficiente!!!");
+					}
+				
+					break;
+			case 2: saldo += valor;
+					System.out.println("Novo Balanço da Conta = " + nf.format(saldo) + " após o depósito de " + nf.format(valor));
+					break;
+		}
+	}
 }
--- src/view/Main.java
+++ src/view/Main.java
@@ -1,9 +1,21 @@
 package view;
 
+import java.util.concurrent.Semaphore;
+
+import java.util.Random;
+
+import controller.ThreadTransacoes;
+
 public class Main 
 {
 	public static void main(String[] args) 
 	{
-		
+		int permissoes = 2;
+		Random R = new Random();
+		Semaphore semaforo = new Semaphore(permissoes);
+		for(int CTA = 0; CTA<20;CTA++) 
+		{
+			Thread t = new ThreadTransacoes((R.nextInt(1)+ 1), (R.nextDouble()*10000), (R.nextDouble()), semaforo);
+		}
 	}
 }
